a478f74678cfb8e732557c45fe500ba2f2cee31d
Cioraz <123grover222@gmail.com>
2023-09-16 21:45:37 +0530

    Week_2

---
+++ Week2/deadManSwitch.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract deadManSwitch {
+    address owner;
+    address presetAddress;
+    uint lastActiveBlock;
+    
+    constructor(address _presetAddress) {
+        owner = msg.sender;
+        _presetAddress = presetAddress;
+        lastActiveBlock = block.number;
+    }
+    
+    function still_alive() public {
+        require(msg.sender == owner);
+        lastActiveBlock = block.number;
+    }
+    
+    function Alive() public view returns (bool) {
+        if(block.number - lastActiveBlock <= 10) return true;
+        else return false;
+    }
+    
+    function transferFunds() public {
+        require(!Alive());
+        uint contractBalance = address(this).balance;
+        payable(presetAddress).transfer(contractBalance); 
+    }
+}
