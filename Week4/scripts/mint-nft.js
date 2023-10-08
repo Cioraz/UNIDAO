95879524d0a0fa3ba22105530244a94bc0cc23b4
Cioraz <123grover222@gmail.com>
2023-10-08 20:40:10 +0530

    Made the NFT

---
+++ Week4/scripts/mint-nft.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const ethers = require('ethers');
+const contract = require("../artifacts/contracts/myNFT.sol/MyNFT.json");
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.AlchemyProvider('sepolia', API_KEY)
+console.log(JSON.stringify(contract.abi));
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = '0x4Bfea85959Ea5d61D2359D7EFee79e83d9F06b5a'
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmcHUxyG6fAq48zWw2ovbCPJeBv9UpgN43ZVicWWTAiRry"
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+    await nftTxn.wait()
+    console.log(`NFT Minted! Check it out at: https://sepolia.etherscan.io/tx/${nftTxn.hash}`)
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
